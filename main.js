44cbbe760c2cb3041112ad868b48acbdd3f47843
Marcel Novák <marcel.novak14@live.com>
2018-12-16 20:53:47 +0100

    init

--- main.js
+++ main.js
@@ -1,51 +1,3 @@
-function unhide(divID) {
-    var item = document.getElementById(divID);
-    console.log('t: ' + JSON.stringify(item));
-
-    var itemWidth = item.width;
-    var itemHeight = item.height;
-    if (item) {
-        if (item.className == 'hidden') {
-            item.className = 'unhidden';
-            if(divID === 'modric') {
-                var text = document.getElementById("winner");
-                 
-                text.className = 'hidden';
-            }
-            if(divID === 'ronaldo') {
-                var text = document.getElementById("second");
-                text.className = 'hidden';
-            }
-            if(divID === 'griezmann') {
-                var text = document.getElementById("third");
-                text.className = 'hidden';
-            }
-        } else {
-            item.className = 'hidden';
-            if(divID === 'modric') {
-                
-                var text = document.getElementById("winner");
-
-                text.style.width = itemWidth + "px";
-                text.style.height = itemHeight + "px";
-                text.className = 'unhidden';
-            }
-            if(divID === 'ronaldo') {
-                var text = document.getElementById("second");
-                text.style.width = itemWidth + "px";
-                text.style.height = itemHeight + "px";
-                text.className = 'unhidden';
-            }
-            if(divID === 'griezmann') {
-                var text = document.getElementById("third");
-                text.style.width = itemWidth + "px";
-                text.style.height = itemHeight + "px";
-                text.className = 'unhidden';
-            }
-        }
-    }
-}
-
 function myFunction(t) {
     t.classList.toggle("change");
     var x = document.getElementById("myTopnav");
@@ -55,3 +7,10 @@ function myFunction(t) {
       x.className = "topnav";
     }
   }
+
+
+  $(".button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".mainSection").offset().top},
+        'slow');
+});
